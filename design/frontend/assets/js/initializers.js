ef24fa8feb94441f6083245c8e3e5bab746029cb
Mike Hook <hook.mike@gmail.com>
2018-07-25 10:08:19 +0100

    Started venue template.

--- design/frontend/assets/js/initializers.js
+++ design/frontend/assets/js/initializers.js
@@ -1,6 +1,26 @@
 // Run javascript after DOM is initialized
-$(document).ready(function() {
+$(document).ready(function () {
 
 	$('#map_canvas').mapit();
+
+	$('#map_canvas').mapit({
+		latitude: 50.9649764,
+		longitude: -0.153036,
+		zoom: 16,
+		type: 'ROADMAP',
+		scrollwheel: false,
+		marker: {
+			latitude: 50.9649764,
+			longitude: -0.153036,
+			icon: '',
+			title: '',
+			open: true,
+			center: true
+		},
+		address: '',
+		// styles:    'GRAYSCALE',
+		locations: [],
+		origins: []
+	});
 
 });
