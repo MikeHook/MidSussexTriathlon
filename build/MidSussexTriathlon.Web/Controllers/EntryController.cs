bc7720056e7e9be521b26f35cf006cd368043dd3
Mike Hook <hook.mike@gmail.com>
2018-08-28 15:11:56 +0100

    Test integration of entry page with Adyen checkoutSDK.

--- build/MidSussexTriathlon.Web/Controllers/EntryController.cs
+++ build/MidSussexTriathlon.Web/Controllers/EntryController.cs
@@ -21,42 +21,7 @@ namespace MidSussexTriathlon.Web.Controllers
             if (!ModelState.IsValid)
                 return CurrentUmbracoPage();
 
-            string merchantAccount = ConfigurationManager.AppSettings["adyenMerchantAccount"];
-
-            string originUrl = string.Format("{0}://{1}{2}", Request.Url.Scheme, Request.Url.Host, Request.Url.Port == 80 ? string.Empty : ":" + Request.Url.Port);
-            string returnUrl = $"{originUrl}/entry/result";
-
-            var paymentSession = new PaymentSessionRequest()
-            {
-                amount = new PaymentSessionRequest.AmountModel()
-                {
-                    currency = "GBP",
-                    value = "3500"
-                },
-                reference = "Order123",
-                merchantAccount = merchantAccount,
-                sdkVersion = "1.3.2",
-                shopperReference = "entrant_Order123",
-                channel = "Web",
-                html = true,
-                origin = originUrl,
-                returnUrl = returnUrl,
-                countryCode = "GB",
-                shopperLocale = "gb_GB"
-            };
-
-            string paymentSessionUrl = "https://checkout-test.adyen.com/v32/paymentSession";
-
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-            using (var client = new HttpClient())
-            {
-                var adyenApiKey = ConfigurationManager.AppSettings["adyenApiKey"];                
-                client.DefaultRequestHeaders.Add("X-API-Key", adyenApiKey);                       
-                var response = await client.PostAsJsonAsync(paymentSessionUrl, paymentSession);
-
-                var responseString = await response.Content.ReadAsStringAsync();
-                var paymentSessionResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<PaymentSessionResponse>(responseString);             
-            }
+       
 
             return RedirectToCurrentUmbracoPage();
         }
---
+++ build/MidSussexTriathlon.Web/Models/Payment/VerifyPaymentResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MidSussexTriathlon.Web.Models.Payment
+{
+    public class VerifyPaymentResponse
+    {
+        public string pspReference { get; set; }
+        public string authResponse { get; set; }
+        public string merchantReference { get; set; }
+        public string paymentMethod { get; set; }
+        public string shopperLocale { get; set; }
+    }
+}
