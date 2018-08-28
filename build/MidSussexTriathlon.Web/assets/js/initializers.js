d715bf7435ecf72e0ccfbb4aeb169a74b62c5a46
Mike Hook <hook.mike@gmail.com>
2018-08-28 15:10:00 +0100

    Added assets from design.

---
+++ build/MidSussexTriathlon.Web/assets/js/initializers.js
@@ -0,0 +1,26 @@
+// Run javascript after DOM is initialized
+$(document).ready(function () {
+
+	$('#map_canvas').mapit();
+
+	$('#map_canvas').mapit({
+		latitude: 50.9649764,
+		longitude: -0.153036,
+		zoom: 16,
+		type: 'ROADMAP',
+		scrollwheel: false,
+		marker: {
+			latitude: 50.9649764,
+			longitude: -0.153036,
+			icon: 'assets/img/marker.png',
+			title: 'Mid Sussex Triathlon Venue',
+			open: false,
+			center: true
+		},
+		address: '',
+		// styles:    'GRAYSCALE',
+		locations: [],
+		origins: []
+	});
+
+});
